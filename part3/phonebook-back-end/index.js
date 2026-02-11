7c40bd46856ecf588416b798cc84062406fc9293
mugaaaaa <yunfaimok59@gmail.com>
2026-02-12 03:20:02 +0800

    feat: add front-end dist

--- part3/phonebook-back-end/index.js
+++ part3/phonebook-back-end/index.js
@@ -18,6 +18,7 @@ morgan.token('body', (request) => {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 app.get('/info', (request, response) => {
     const date = new Date()
